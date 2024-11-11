7e5744748f08f62e5dd82184a60cf00522ae1f7a
Kaylee Odom <kaylee.odom@students.fhu.edu>
2024-11-11 14:52:31 -0600

    small wrk

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 const carouselContainer = document.querySelector('.carousel-container');
-    const carouselImages = document.querySelectorAll('.carousel-image');
-    const prevBtn = document.querySelector('.prev-btn');
-    const nextBtn = document.querySelector('.next-btn');
+const carouselImages = document.querySelectorAll('.carousel-image');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
 
-    let currentIndex = 0;
-    const numImages = carouselImages.length;
+let currentIndex = 0;
+const numImages = carouselImages.length;
 
-    function updateCarousel() {
-      carouselContainer.style.transform = `translateX(-${currentIndex * (100 / numImages)}%)`;
-    }
+function updateCarousel() {
+  carouselContainer.style.transform = `translateX(-${currentIndex * (100 / numImages)}%)`;
+}
 
-    function goToPreviousImage() {
-      currentIndex = (currentIndex - 1 + numImages) % numImages;
-      updateCarousel();
-    }
+function goToPreviousImage() {
+  currentIndex = (currentIndex - 1 + numImages) % numImages;
+  updateCarousel();
+}
 
-    function goToNextImage() {
-      currentIndex = (currentIndex + 1) % numImages;
-      updateCarousel();
-    }
+function goToNextImage() {
+  currentIndex = (currentIndex + 1) % numImages;
+  updateCarousel();
+}
 
-    prevBtn.addEventListener('click', goToPreviousImage);
-    nextBtn.addEventListener('click', goToNextImage);
+prevBtn.addEventListener('click', goToPreviousImage);
+nextBtn.addEventListener('click', goToNextImage);
 
-    // Automatically move to the next image every 1 seconds
-    setInterval(goToNextImage, 1000);
+// Automatically move to the next image every 1 seconds
+setInterval(goToNextImage, 1000);
 
 
-    // sponsor drop down
-    const infoListItems = document.querySelectorAll('.info-list-item');
+// sponsor drop down
+const infoListItems = document.querySelectorAll('.info-list-item');
 
-    infoListItems.forEach(item => {
-      item.querySelector('h3').addEventListener('click', () => {
-        item.classList.toggle('expanded');
-      });
-    });
+infoListItems.forEach(item => {
+  item.querySelector('h3').addEventListener('click', () => {
+  item.classList.toggle('expanded');
+  });
+});
+
