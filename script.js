c85b5a0420318ba3a4a76a2cc31f12914b59c76b
AveryH6 <89232897+AveryH6@users.noreply.github.com>
2024-11-20 00:44:27 -0600

    fixed carousel

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
-const carouselContainer = document.querySelector('.carousel-container');
-const carouselImages = document.querySelectorAll('.carousel-image');
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-
-let currentIndex = 0;
-const numImages = carouselImages.length;
-
-function updateCarousel() {
-  carouselContainer.style.transform = `translateX(-${currentIndex * (100 / numImages)}%)`;
-}
-
-function goToPreviousImage() {
-  currentIndex = (currentIndex - 1 + numImages) % numImages;
-  updateCarousel();
-}
-
-function goToNextImage() {
-  currentIndex = (currentIndex + 1) % numImages;
-  updateCarousel();
-}
-
-prevBtn.addEventListener('click', goToPreviousImage);
-nextBtn.addEventListener('click', goToNextImage);
-
-// Automatically move to the next image every 1 seconds
-setInterval(goToNextImage, 1000);
+var copy = document.querySelector('.logos-slide').cloneNode(true)
+document.querySelector(".logos").appendChild(copy)
 
 
 
@@ -37,6 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
       hamburger.classList.toggle("active");
   });
 });
+
+
+
 
 
 
